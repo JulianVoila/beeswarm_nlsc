640718f9e55f1d7181d873ff7ab5823c6f5767dd
JulianVoila <119368018+JulianVoila@users.noreply.github.com>
2024-05-22 14:18:21 -0400

    Basic structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,72 @@
 <script>
-  import Example from "$components/Example.svelte";
   import data from "$data/data.js";
   console.log(data);
+
+  import { forceSimulation, forceY, forceX, forceCollide } from "d3-force";
+  import { scaleLinear, scaleBand } from "d3-scale";
+  import { extent } from "d3-array";
+  import { each } from "svelte/internal";
+  import { mean, rollups } from "d3-array";
+
+  const RADIUS = 5;
+
+  $: simulation = forceSimulation(data)
+    .force(
+      "x",
+      forceX()
+        .x((d) => xScale(d.happiness))
+        .strength(0.08),
+    )
+    .force(
+      "y",
+      forceY()
+        .y((d) => yScale(d.continent))
+        .strength(0.02),
+    )
+    .force("collide", forceCollide().radius(RADIUS));
+
+  $: nodes = simulation.nodes();
+
+  // console.log(simulation);
+
+  let width = 400,
+    height = 400;
+
+  const margin = {
+    top: 0,
+    right: 0,
+    bottom: 20,
+    left: 0,
+  };
+
+  $: innerWidth = width - margin.left - margin.right;
+  let innerHeight = height - margin.top - margin.bottom;
+
+  const continents = rollups(
+    data,
+    v => mean(v, d => d.happiness),
+    d => d.continent
+  ) // Group data by continent and return the group-wide mean
+    .sort((a, b) => a[1] - b[1]) // Sort according to value
+    .map(d => d[0]); // Grab the continent name
+
+  $: xScale = scaleLinear()
+    // .domain(extent(data, (d) => Math.round(d.happiness)))
+    .domain([0, 10])
+    .range([0, innerWidth]);
+
+  let yScale = scaleBand()
+    .domain(continents)
+    .range([innerHeight, 0])
+    .paddingOuter(0.5);
 </script>
 
-<main>
-  <h1>Let's make a chart 😎</h1>
-  <h2>
-    Get started by deleting all of the contents in <pre>App.svelte</pre>
-    🗑
-  </h2>
-  <Example />
-  <footer>
-    For help, <a
-      href="https://twitter.com/CL_Rothschild"
-      target="_blank"
-      rel="noopener noreferrer">DM Connor on Twitter ✉️</a
-    >
-  </footer>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background: #f0f0f0;
-  }
-
-  h1 {
-    font-size: 3rem;
-    margin-bottom: 1rem;
-    font-weight: 700;
-  }
-
-  h2 {
-    font-size: 1.5rem;
-    color: #333;
-    margin-bottom: 2rem;
-    line-height: 1.5;
-  }
-
-  pre {
-    padding: 1px 6px;
-    display: inline;
-    margin: 0;
-    background: #ffb7a0;
-    border-radius: 3px;
-  }
-
-  a {
-    color: #ff3e00;
-    text-decoration: inherit;
-  }
-
-  footer {
-    font-size: 1rem;
-    color: #333;
-  }
-</style>
+<div class="chart-container" bind:clientWidth={width}>
+  <svg {width} {height}>
+    <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
+      {#each nodes as node}
+        <circle cx={node.x} cy={node.y} r={RADIUS} fill="steelblue" />
+      {/each}
+    </g>
+  </svg>
+</div>
