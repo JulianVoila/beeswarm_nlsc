4f85321f4ef5856e8f37139213a36d1a895dd6ba
JulianVoila <119368018+JulianVoila@users.noreply.github.com>
2024-06-11 16:04:01 -0400

    Add Axes, Titles, and Legends to Svelte Beeswarm Visualization

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,25 @@
   console.log(data);
 
   import { forceSimulation, forceY, forceX, forceCollide } from "d3-force";
-  import { scaleLinear, scaleBand } from "d3-scale";
+  import { scaleLinear, scaleBand, scaleOrdinal, scaleSqrt } from "d3-scale";
   import { extent } from "d3-array";
   import { each } from "svelte/internal";
   import { mean, rollups } from "d3-array";
 
-  const RADIUS = 5;
 
-  $: simulation = forceSimulation(data)
-    .force(
+  const RADIUS = 5;
+  const simulation = forceSimulation(data)
+
+
+  let nodes = [];
+  simulation.on("tick", () => {
+    nodes = simulation.nodes();
+  });
+
+
+$: {
+  simulation
+  .force(
       "x",
       forceX()
         .x((d) => xScale(d.happiness))
@@ -21,11 +31,13 @@
       "y",
       forceY()
         .y((d) => yScale(d.continent))
-        .strength(0.02),
+        .strength(0.04),
     )
-    .force("collide", forceCollide().radius(RADIUS));
-
-  $: nodes = simulation.nodes();
+    .force("collide", forceCollide().radius(d => radiusScale(d.happiness)))
+    .alpha(1)
+    .alphaDecay(0.0005)
+    .restart()
+}
 
   // console.log(simulation);
 
@@ -42,6 +54,7 @@
   $: innerWidth = width - margin.left - margin.right;
   let innerHeight = height - margin.top - margin.bottom;
 
+
   const continents = rollups(
     data,
     v => mean(v, d => d.happiness),
@@ -50,22 +63,38 @@
     .sort((a, b) => a[1] - b[1]) // Sort according to value
     .map(d => d[0]); // Grab the continent name
 
+
   $: xScale = scaleLinear()
     // .domain(extent(data, (d) => Math.round(d.happiness)))
-    .domain([0, 10])
+    .domain([1, 9])
     .range([0, innerWidth]);
 
   let yScale = scaleBand()
     .domain(continents)
     .range([innerHeight, 0])
     .paddingOuter(0.5);
+
+  const colorRange = ["#dda0dd", "#fe7f2d", "#fcca46", "#a1c181", "#619b8a", "#eae2b7"];
+    const colorScale = scaleOrdinal()
+    .domain(continents)
+    .range (colorRange)
+
+   $: radiusScale = scaleSqrt()
+      .domain([0,10])
+      .range(width < 568 ? [2,6]:[3,8])
+
+  import AxisX from "$components/AxisX.svelte";
+
 </script>
+
 
 <div class="chart-container" bind:clientWidth={width}>
   <svg {width} {height}>
     <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
+      <AxisX xScale={xScale} height={innerHeight} width={innerWidth}/>
       {#each nodes as node}
-        <circle cx={node.x} cy={node.y} r={RADIUS} fill="steelblue" />
+        <circle cx={node.x} cy={node.y} r={radiusScale(node.happiness)} 
+        fill={colorScale(node.continent)} stroke="black" />
       {/each}
     </g>
   </svg>
---
+++ src/components/AxisX.svelte
@@ -0,0 +1,21 @@
+<script>
+export let xScale;
+export let width;
+export let height;
+
+$: console.log({xScale, width, height})
+
+$: ticks = xScale.ticks(4)
+
+
+</script>
+
+
+<g class="axis x">
+{#each ticks as tick}
+    <g class="tick" transform="translate({xScale(tick)}, 0)"> 
+        <text x="4" y={height}>{tick}</text>
+        <line x1="0" x2="0" y1="0" y2={height} stroke="hsla(215, 15%, 91%, 1)" />
+    </g>
+{/each}
+</g>
